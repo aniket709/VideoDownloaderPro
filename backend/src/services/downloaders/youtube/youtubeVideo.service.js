e4f22802c19f8628717bbf95b3016202c6cfd48a
aniket <aniket.ranjan709@gmail.com>
2026-09-16 12:14:16 +0530

    fix youtuve audio service

--- backend/src/services/downloaders/youtube/youtubeVideo.service.js
+++ backend/src/services/downloaders/youtube/youtubeVideo.service.js
@@ -1,4 +1,23 @@
-// backend/src/services/downloaders/youtube/youtubeVideo.service.js
+// // backend/src/services/downloaders/youtube/youtubeVideo.service.js
+// import BaseDownloader from '../baseDownloader.js';
+// import youtubedl from 'youtube-dl-exec';
+// import path from 'path';
+
+// export default class YoutubeVideoService extends BaseDownloader {
+//     async download() {
+//         console.log("YouTube URL received:", this.url);
+//         const outputPath = path.join(this.outputDir, `youtube_video_${Date.now()}.mp4`);
+//         await youtubedl(this.url, {
+//             output: outputPath,
+//             format: "bv*+ba/b",
+//             addHeader: ["referer:youtube.com"],
+//             extractorArgs: "youtube:player_client=android",
+//             mergeOutputFormat: 'mp4'
+//         });
+//         return { success: true, filePath: outputPath };
+//     }
+// }
+
 import BaseDownloader from '../baseDownloader.js';
 import youtubedl from 'youtube-dl-exec';
 import path from 'path';
@@ -6,14 +25,22 @@ import path from 'path';
 export default class YoutubeVideoService extends BaseDownloader {
     async download() {
         console.log("YouTube URL received:", this.url);
-        const outputPath = path.join(this.outputDir, `youtube_video_${Date.now()}.mp4`);
+
+        const outputPath = path.join(
+            this.outputDir,
+            `youtube_video_${Date.now()}.mp4`
+        );
+
         await youtubedl(this.url, {
             output: outputPath,
             format: "bv*+ba/b",
             addHeader: ["referer:youtube.com"],
-            extractorArgs: "youtube:player_client=android",
             mergeOutputFormat: 'mp4'
         });
-        return { success: true, filePath: outputPath };
+
+        return {
+            success: true,
+            filePath: outputPath
+        };
     }
 }
